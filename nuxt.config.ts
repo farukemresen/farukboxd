097d3331e281341e64d4ff9eb0497bc727186718
FarukEmreŞen <faruksen@ogr.bandirma.edu.tr>
2025-07-24 13:58:38 +0300

    ilk

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@pinia/nuxt'],
+  css: ['~/assets/css/main.css'],
+})
