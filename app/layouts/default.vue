90775600b18e461cf6ab3649c1c413f7dfb3b428
FarukEmreŞen <faruksen@ogr.bandirma.edu.tr>
2025-07-29 09:46:28 +0300

    tema seçeneği eklendi.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,14 @@
 
 <template>
   <div class="min-h-dvh bg-white dark:bg-neutral-900">
+    <USwitch
+      color="secondary"
+      checked-icon="i-heroicons-sun"
+      unchecked-icon="i-heroicons-moon"
+      variant="ghost"
+      class="hover:scale-110 transition-all absolute top-4 right-4 z-50 flex items-center gap-2"
+      @click="toggleTheme"
+    />
     <div class="text-neutral-900 dark:text-white">
       <AppHeader />
     </div>
